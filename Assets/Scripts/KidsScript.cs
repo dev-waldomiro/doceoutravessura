c248d6275a268576cc69847dfefd7dfcd97ab6f0
Waldomiro Barbosa Romão <devwaldomiroromao@gmail.com>
2018-11-02 14:44:44 -0300

    things!

---
+++ Assets/Scripts/KidsScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KidsScript : MonoBehaviour {
+
+	Random rnd = new System.Random();
+
+	void startGiving (Input.KeyDown("enter")){
+
+		int persona = rnd.Next(1, 4);
+		int indexNo = rnd.Next(1, 14);
+		int sprite = rnd.Next(1, 14);
+
+	}
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	public int money = 25;
+	public int bombom = 10;
+	public int bala = 20;
+	public int doce = 2;
+
+	public Text playerText;
+
+	void setPlayerText(){
+		playerText.text = "Dinheiros: " + money.ToString() + " Balas: " + bala.ToString();
+	}
+	
+	void Start () {
+		setPlayerText();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		setPlayerText();
+		if(money == 0)
+			Application.LoadLevel("Entrega");
+	}
+}
