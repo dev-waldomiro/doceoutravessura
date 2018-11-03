6b4447b092ecd910b12ead0a59dd7487d0d5de18
Waldomiro Barbosa Romão <devwaldomiroromao@gmail.com>
2018-11-03 00:09:23 -0300

    new stuff

---
+++ .vscode/Assets/Scripts/CandyScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandyScript : MonoBehaviour {
+
+	public int cost;
+	public bool bombom = false;
+	public bool cupck = false;
+	public bool bala = false;
+	public PlayerScript inventory;
+
+	public void getMoney(){
+		if(cost <= inventory.money)
+			inventory.money -= cost;
+
+		if(cupck && cost <= inventory.money)
+			inventory.cupck += 1;
+		else if (bombom && cost <= inventory.money)
+			inventory.bombom += 3;
+		else if (bala && cost <= inventory.money)
+			inventory.bala += 5;
+
+	}
+
+	// Use this for initialization
+	void Awake () {
+		inventory = GameObject.FindWithTag("Player").GetComponent<PlayerScript>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ .vscode/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	public int money = 25;
+	public int bombom = 12;
+	public int bala = 28;
+	public int cupck = 10;
+
+	public Text playerText;
+
+	void setPlayerText(){
+		playerText.text = "Dinheiros: " + money.ToString() + " Balas: " + bala.ToString();
+	}
+	
+	void Start () {
+		setPlayerText();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		setPlayerText();
+		if(money == 0)
+			Application.LoadLevel("Entrega");
+
+		PlayerPrefs.SetInt("bombom", bombom);
+		PlayerPrefs.SetInt("bala", bala);
+		PlayerPrefs.SetInt("cupck", cupck);
+	}
+}
