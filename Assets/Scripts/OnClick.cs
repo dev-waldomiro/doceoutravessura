6f5a356715e3830d154818e52f6e953ced76155f
Gustavo-PCzin\Gustavo <gugaksouza@gmail.com>
2018-11-03 03:19:59 -0300

    no message

---
+++ Assets/Scripts/OnClick.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OnClick : MonoBehaviour {
+
+	public void Load()
+	{
+		SceneManager.LoadScene(1);
+	}
+
+	void update()
+	{
+	}
+}
